3f1e09c6d9ccbc8ac349d2a4dc6c32c925a0f61f
David Coats <david@getsafetycone.com>
2021-04-30 14:37:29 -0700

    Add R5T.D0053.Base service definitions library. Add R5T.Aalborg.Abstractions project-reference dependency. Remove extraneous R5T.D0053 library.

---
+++ source/R5T.D0053.Base/Code/Services/Definitions/IGmailAuthenticationProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using R5T.Aalborg;
+
+
+namespace R5T.D0053
+{
+    public interface IGmailAuthenticationProvider
+    {
+        Task<IAuthentication> GetGmailAuthentication();
+    }
+}
