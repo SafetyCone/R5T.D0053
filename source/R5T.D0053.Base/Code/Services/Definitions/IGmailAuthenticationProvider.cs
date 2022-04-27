b22a373eeda71f71817e3ff0ddbf72e6442b1edc
davidcoats <david@getsafetycone.com>
2022-04-27 15:27:42 -0700

    Interim changes.

--- source/R5T.D0053.Base/Code/Services/Definitions/IGmailAuthenticationProvider.cs
+++ source/R5T.D0053.Base/Code/Services/Definitions/IGmailAuthenticationProvider.cs
@@ -3,10 +3,13 @@ using System.Threading.Tasks;
 
 using R5T.Aalborg;
 
+using R5T.T0064;
+
 
 namespace R5T.D0053
 {
-    public interface IGmailAuthenticationProvider
+    [ServiceDefinitionMarker]
+    public interface IGmailAuthenticationProvider : IServiceDefinition
     {
         Task<IAuthentication> GetGmailAuthentication();
     }
